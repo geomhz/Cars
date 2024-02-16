50bb107413f3f5f85bb6b2ec3c70e16c132dfd37
Geovanne Murata <espacozanata1@gmail.com>
2024-02-16 13:41:33 -0300

    urls, views e templates

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from cars.models import Car
 
-# Create your views here.
+def cars_view(request):
+    cars = Car.objects.all() # mesma coisa que o "SELECT * FROM xx"
+
+    return render(
+        request,
+        'cars.html',
+        {'cars': {'model': 'Ferrari GTB'} }
+        )
+
+'''
+Cars.objects.all() # mesma coisa que o "SELECT * FROM xx" e retorna uma Queryset
+
+render pode ter parametros:
+    request
+    templates (.html)    
+    context
+'''
