55c35574d497479b3b77eb2e86b6220638b552a4
Geovanne Murata <espacozanata1@gmail.com>
2024-02-17 00:07:44 -0300

    views.generic: ListView e CreateView

--- cars/views.py
+++ cars/views.py
@@ -1,28 +1,29 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render,redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 
 from django.views import View
+from django.views.generic import ListView, CreateView
 
-class CarsView(View):
-
-    def get(self, request):
-        cars = Car.objects.all().order_by('brand__name')
-        search = request.GET.get('search')
-
+# Listando carros com CBVs
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+    
+    # Filtros + ordenação por brand e model
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('brand__name', 'model')
+        search = self.request.GET.get('search')
         if search:
-            cars = Car.objects.filter(model__icontains=search).order_by('model')
-        
-        return render(request, 'cars.html', {'cars': cars })
+            cars = cars.filter(model__icontains=search)
+        return cars
 
-class NewCarView(View):
-    def get(self, request):
-        new_car_form = CarModelForm()
-        return render(request, 'new_car.html', { 'new_car_form': new_car_form })
-
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render(request, 'new_car.html', { 'new_car_form': new_car_form })
+# Registrando carros com CBVs
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
